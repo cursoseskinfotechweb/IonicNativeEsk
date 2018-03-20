32b5b34507c932b4c80a613beddc09fca211e726
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-20 14:08:44 -0300

    Criando novo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -8,6 +8,11 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
+
+  @ViewChild(Nav) nav: Nav;
+
+  pages: [{title: string, component: any}]
+  
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -18,5 +23,10 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  openPage(page: {title: string, component: any}): void {
+    this.nav.push(page.component);
+  }
+
 }
 
