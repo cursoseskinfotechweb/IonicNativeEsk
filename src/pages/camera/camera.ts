955455b921345779cb314f2ae956fc712e2b5952
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-21 14:33:22 -0300

    Camera: implementando os métodos

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-camera',
@@ -7,11 +8,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CameraPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public actionSheetCtrl: ActionSheetController,
+    public camera: Camera,
+    public navCtrl: NavController, 
+    public navParams: NavParams
+  ) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CameraPage');
+  onActionSheet(): void {
+    this.actionSheetCtrl.create({
+      title: 'Select image source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel'
+        }
+      ]
+    }).present();
   }
 
+  takePicture(sourceType: number): void {
+
+    let cameraOptions: CameraOptions = {
+      correctOrientation: true,
+      quality: 100,
+      saveToPhotoAlbum: false,
+      sourceType: sourceType
+    };
+
+    this.camera.getPicture(cameraOptions)
+      .then((fileUri: string) => {
+        console.log('Photo: ', fileUri);
+      }).catch((error: Error) => console.log('Camera error: ', error));
+  }
 }
