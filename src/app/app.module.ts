22533731bb12971e3798dd472b06e9d18d1bfa47
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-23 15:24:41 -0300

    File e FilePath: Instalando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -32,6 +34,8 @@ import { CameraPage } from '../pages/camera/camera';
   providers: [
     BarcodeScanner,    
     Camera,
+    File,
+    FilePath,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
